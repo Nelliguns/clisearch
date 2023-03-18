2779c4777fa4ea4b19dc2cd35d5011a3e7a92ed0
Liam Nelligan <liamnelligan000@gmail.com>
2023-03-18 21:59:41 +0100

    basic get request in place

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn main() ->  Result<(), Box<dyn std::error::Error>> {
+    let url = "https://google.com/search";
+    let query = "rustbook";
+    let params = [
+        ("q", query)
+    ];
+    let url = reqwest::Url::parse_with_params(url, &params)?;
+    let res = reqwest::blocking::get(url)?;
+
+    println!("Rust book search: = {:#?}", res.text().unwrap());
+
+    Ok(())
 }
