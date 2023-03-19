3aa9d7e15a15a6952d7205c3ab63fc44fca8a81f
Liam Nelligan <liamnelligan000@gmail.com>
2023-03-19 11:29:21 +0100

    Partial parsing in place

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+use scraper::Html;
+
 fn main() ->  Result<(), Box<dyn std::error::Error>> {
     let url = "https://google.com/search";
-    let query = "rustbook";
+    let query = "rust book";
     let params = [
         ("q", query)
     ];
     let url = reqwest::Url::parse_with_params(url, &params)?;
-    let res = reqwest::blocking::get(url)?;
+    let res = reqwest::blocking::get(url).unwrap().text().unwrap();
 
-    println!("Rust book search: = {:#?}", res.text().unwrap());
+    // println!("Rust book search: = {:#?}", res);
+
+    let document = Html::parse_document(&res);
+    // println!("{:#?}", document);
+
+    let link_selector = scraper::Selector::parse("div.yuRUbf>a").unwrap();
+    // println!("{:#?}", link_selector);
+
+    let links = document.select(&link_selector).map(|x| x.inner_html());
+    println!("{:#?}", links);
+
+    links
+        .zip(1..3)
+        .for_each(|(item, number)| println!("{}. {}", number, item));
 
     Ok(())
 }
